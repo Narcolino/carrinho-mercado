41f2136c42b5efa95ea69ff0798b768a4cd50862
Marco Antonio <marcosanto5550123@outlook.com>
2024-12-18 16:19:22 -0400

    Finalizando o projeto de carrinho de mercado

--- js/app.js
+++ js/app.js
@@ -1,28 +1,42 @@
 function adicionar() {
-    let opcoes = document.getElementById('produto');
-    let produto = opcoes.value;
+    let produto = document.getElementById('produto').value;
     let quantidade = document.getElementById('quantidade').value;
 
-    let posicaoHifen = produto.indexOf('-');
-    let valor = produto.slice(posicaoHifen + 4).trim();
+    let valor = parseInt(produto.split('R$')[1]);
+    let textoProd = produto.split('-')[0];
 
-    let textoProd = produto.substring(0,posicaoHifen).trim();
-
-    escreverCarrinho(textoProd,quantidade,valor);
+    if (quantidade == 0){
+        alert('Quantidade igual a zero, por favor selecione alguma quantidade para o produto ');
+        document.getElementById('produto').value = 'Fone de ouvido - R$100';
+    }else{
+        escreverCarrinho(textoProd,quantidade,valor);
+        calcular(valor,quantidade);
+        document.getElementById('quantidade').value = '';
+        document.getElementById('produto').value = 'Fone de ouvido - R$100';
+    }
 
 }
 
 function escreverCarrinho (prod,quant,valor){
     let lista = document.getElementById ('lista-produtos');
     let itens = lista.querySelector('section');
-    itens.innerHTML += `<span class="texto-azul">${quant}x</span> ${prod} <span class="texto-azul">R$${valor}</span>`
+    itens.innerHTML += `<br><span class="texto-azul">${quant}x</span> ${prod} <span class="texto-azul">R$${valor}</span>`
 }
 
 function limpar(){
+    let lista = document.getElementById ('lista-produtos');
+    let itens = lista.querySelector('section');
+    itens.innerHTML = '';
+    document.getElementById('valor-total').innerHTML = '000';
 
 }
 
-function calcular(){
+function calcular(valor,quant){
+    let soma = valor*quant;
+    let carrinho_total = document.getElementById('valor-total');
+    let total = parseInt(carrinho_total.textContent);
+    total += soma;
+    carrinho_total.innerHTML = `${total}`;
 
 }
 
